6213b7b5139afa4d3c1f2b448e122c64cf174a7e
Yang-Mini <leeyang1991@gmail.com>
2022-12-15 05:00:33 +0800

    0.0.5

--- README.md
+++ README.md
@@ -8,3 +8,12 @@ Inspired by [xycmap](https://github.com/rbjansen/xycmap).
 
 #### Installation
 `pip install xymap`
+
+#### todo list:
+1. provide color ramps options
+2. add band palette to tif
+3. add user defined color ramps
+4. interpolate color ramps
+5. vector format support
+6. triangle mesh support
+
--- xymap/_xymap.py
+++ xymap/_xymap.py
@@ -1,4 +1,14 @@
 # coding=utf-8
+'''
+todo list:
+1. provide color ramps options
+2. add band palette to tif # https://gis.stackexchange.com/questions/325615/store-geotiff-with-color-table-python
+3. add user defined color ramps
+4. interpolate color ramps
+5. vector format support
+6. triangle mesh support
+'''
+
 # from lytools import *
 import xycmap
 import numpy as np
